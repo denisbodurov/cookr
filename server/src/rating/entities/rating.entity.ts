f30bb916355facb1c22120cfabb05d67feda932b
RottenZelka <mishoitedis@gmail.com>
2024-07-01 15:48:31 +0300

    fix rate entity

--- server/src/rating/entities/rating.entity.ts
+++ server/src/rating/entities/rating.entity.ts
@@ -19,30 +19,5 @@ export class RatingEntity {
   @OneToOne(() => RecipeEntity, (recipe) => recipe.recipe_id)
   @JoinColumn()
   rated_id: number;
-
-  @Column()
-  @IsEmail()
-  email: string;
-
-  @Column({default: ''})
-  bio: string;
-
-  @Column({default: ''})
-  image: Blob;
-
-  @Column()
-  @Length(6, 30, { message: 'The password must be at least 6 but not longer than 30 characters' })
-  @IsNotEmpty({ message: 'The password is required' })
-  password!: string;
-
-  @BeforeInsert()
-  async hashPassword() {
-    this.password = await argon2.hash(this.password);
-  }
-
-  @OneToMany(type => RecipeEntity, recipe => recipe.author_id)
-  @JoinTable()
-  recipes: RecipeEntity[];
-
   
 }
