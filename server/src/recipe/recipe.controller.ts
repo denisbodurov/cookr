17f07cdea88d0d2134d43f45ea6bcac37c49c6b2
RottenZelka <mishoitedis@gmail.com>
2024-07-01 12:30:59 +0300

    asd

---
+++ server/src/recipe/recipe.controller.ts
@@ -0,0 +1,10 @@
+import { Post, Body } from '@nestjs/common';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
+
+export class RecipeController{
+    @Post('recipe')
+    async create(@Body('recipe') recipeData: CreateRecipeDto) {
+        return this.userService.create(recipeData);
+    }
+    
+}
