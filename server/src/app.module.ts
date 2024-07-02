5850f536bc362cd7b5e3dfd3793b9038cdca121d
RottenZelka <mishoitedis@gmail.com>
2024-07-02 13:52:03 +0300

    sql script used for db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -18,11 +18,11 @@ import { LikedRecipesModule } from './liked_recipes/liked_recipes.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: "postgres",
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        host: '127.0.0.1',
+        port: 5432,
+        username: 'postgres',
+        password: '1234',
+        database: 'cookr',
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
 
