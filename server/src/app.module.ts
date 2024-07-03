ecb9c5b8bee844872c33afdefb6082f595f65d69
RottenZelka <mishoitedis@gmail.com>
2024-07-03 10:21:07 +0300

    config of database in new file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,27 +8,14 @@ import { RecipesModule } from './recipes/recipes.module';
 import { UsersModule } from './users/users.module';
 import { RatingsModule } from './ratings/ratings.module';
 import { LikedRecipesModule } from './liked_recipes/liked_recipes.module';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-
-      }),
-      inject: [ConfigService]
-  }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     AuthModule,
     RecipesModule,
     UsersModule,
---
+++ server/src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+    useFactory: async(): Promise<TypeOrmModuleOptions> => {
+        console.log(process.env.DB_PORT);
+        return {
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            logging: true
+        }
+    }
+}
