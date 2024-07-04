1f30e4564a714cc20daf736e0039f179f9436de4
Denis Bodurov <denisbodurov05@gmail.com>
2024-07-04 09:36:02 +0300

    Switched ormconfig to using env variables

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -1,14 +1,17 @@
 import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // This line is necessary if you're not using a package that automatically loads environment variables.
 
 export const connectionSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 6432,
-  username: "postgres",
-  password: "1234",
-  database: "cookr",
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   logging: true,
-  synchronize: false,
+  synchronize: false, // It's recommended to not use synchronize: true in production.
   name: "default",
   entities: ["src/**/*.entity.ts"],
   migrations: ["src/migrations/*.ts"],
