7adc0c0cd7191dba005c304359edd4e40984c4ca
Patrazzo <petarpetrov092@gmail.com>
2024-07-10 07:51:57 +0300

    Create auth files in the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/home.tsx";
-import AddNew from "./pages/addNew.tsx";
-import Login from "./pages/login.tsx";
-import Register from "./pages/register.tsx";
-import Header from "./components/header.tsx";
-import Footer from "./components/footer.tsx";
-import RecipePage from "./pages/recipe-page.tsx";
-import AllRecipes from "./pages/all-recipes.tsx";
-import ProfilePage from "./pages/profile-page.tsx";
-const App: React.FC = () => {
-  
+import AuthProvider from "./provider/authProvider.tsx";
+import Routes from "./routes";
+
+function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/add-new" element={<AddNew />} />
-          <Route path="/recipe-page" element={<RecipePage />} />
-          <Route path="/all-recipes" element={<AllRecipes />} />
-          <Route path="/my-profile" element={<ProfilePage />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
-};
+}
 
 export default App;
